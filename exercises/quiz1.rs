1d873334bb9e3f44eedbce076c8eaaccc6005e11
yhamacnu <64330142+yhamacnu@users.noreply.github.com>
2024-04-13 20:44:35 +0000

    my rustling commit

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(input: i32) -> i32 {
+    if input > 40 { input } else { input * 2 }
+}
 
 // Don't modify this function!
 #[test]
